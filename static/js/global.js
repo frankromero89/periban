449668e63b8aedaff277a6fe49f941ec097ab9a4
Frank Romero <89.frankromero@gmail.com>
2023-04-17 22:34:51 -0600

    add countdown timer

--- static/js/global.js
+++ static/js/global.js
@@ -57,6 +57,7 @@ $(document).ready(async function() {
     actionPig();
     showMenuMobile();
     buttonLocation();
+    timer();
 });
 
 
@@ -182,4 +183,25 @@ function buttonLocation(){
     $(buttonLoc).click(function() {
         getCurrentLocation()
     });   
+}
+
+var countDownDate = new Date("2023-04-20 08:30:00").getTime();
+
+function timer(){
+    setInterval(function() {
+
+        var now = new Date().getTime();
+        var timeLeft = countDownDate - now;
+
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("mins").innerHTML = minutes;
+        document.getElementById("secs").innerHTML = seconds;
+
+    }, 1000)
 }
